dd4d48e087f3ba81fbc2fa5a5046a5f49863d04a
Rachel <racheljay42@gmail.com>
2023-06-03 19:04:10 -0700

    add example and additional property

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,11 +1,12 @@
 package iteration
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -15,6 +16,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 10)
+	fmt.Println(repeat)
+	// Output: aaaaaaaaaa
 }
