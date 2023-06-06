24a16bb861e4a93d3bb19c8a8e2e724285be7343
Rachel <racheljay42@gmail.com>
2023-06-06 15:42:24 -0700

    error handling on withdraw method

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -27,6 +30,10 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
 	w.balance -= amount
+	return nil
 }
