2c9061553675a69d7326c21effe0f9158dbce429
Rachel <racheljay42@gmail.com>
2023-06-09 16:01:09 -0700

    use io.Writer instead of bytes for more general use

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	//Fprint is what is called with fmt.Printf
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
