2da6e96528f0f920e6b870a2e356e28798815f0c
Rachel <racheljay42@gmail.com>
2023-06-08 22:12:36 -0700

    more add logic and dictionaryerr type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,18 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
 var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+// makes errors constant, reusable and immutable
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -19,6 +24,16 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 func (d Dictionary) Add(word, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
 	return nil
 }
